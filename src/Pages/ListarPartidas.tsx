743d60c5217168660db3797cd43c97601d253d5b
Brendon Genssinger <brendon.genssinger@engegraph.com.br>
2022-04-28 22:52:02 -0300

    ajustes listar partida

--- src/Pages/ListarPartidas.tsx
+++ src/Pages/ListarPartidas.tsx
@@ -1,14 +1,70 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
 import React,{useState} from "react";
 
 export function ListarPartidas()
 {
 
 
-    const[listaContras, setListaContras] = useState<string[]>();
-    const[listaJogadores,SetListJogadores] = useState<string[]>();
-    const[jogadoresJogando,SetJogadoresJogando] = useState<number[]>();
-    const[jogadoresDeFora,SetJogadoresDeFora] = useState<number[]>();
+    const listaContras =new Array<number>();
+
+    const listaJogadores = new Array<string>();
+
+    const[listaTextNomesDuplas,setListTextoNomes] = useState<Array<string>>([]);
+    const[jogadoresJogando,setJogadoresJogando] = useState<number[]>([0,1,2,3]);
+    const[jogadoresDeFora,SetJogadoresDeFora] = useState<number[]>([4,5,6]);
+    const [myArray, updateMyArray] = useState<number[]>();    
+    const [inicioJogadores,setInicioJogadores] = useState([1,2,3,4]);
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => {
+    setOpen(false);
+    };
+    let valorDuplo = 1;
+
+
+    function MontarDupla (){
+
+        if(listaContras && listaJogadores){
+            let dupla1 = "";
+            let dupla2 = "";
+            let dupla1ou2 = 1;
+            let jogadores= new Array<string>();
+            for (let index = 0; index < listaContras.length; index=index+2) 
+            {
+
+                if(listaContras[index]?.valueOf() || listaContras[index+1]?.valueOf())
+                {
+                    var jgA = listaContras[index]?.valueOf();
+                    var jgB = listaContras[index+1]?.valueOf();
+
+                    if(listaJogadores == null || listaJogadores.length ==0)
+                    {
+                        getJogadores();
+                    }
+                    
+                    if(listaJogadores[jgA]?.valueOf() || listaJogadores[jgB]?.valueOf())
+                    {
+                            var jogadorA = listaJogadores[jgA]?.valueOf();
+                            var jogadorB = listaJogadores[jgB]?.valueOf();
+                            if(dupla1ou2==1){
+                                dupla1 = jogadorA+" e " + jogadorB;
+                                dupla1ou2 = 2;
+                            }
+                            else{
+                                dupla2 =" X " + jogadorA+" e " + jogadorB;
+                                dupla1ou2 = 1;
+                                if(jogadorA && jogadorB)
+                                    jogadores.push(dupla1+dupla2);                    
+                            }
+                    }
+
+                } 
+            }
+            setListTextoNomes(jogadores);
+        }
+
+    }
+
+    
 
     function getJogador(number:string)
     {
@@ -21,82 +77,99 @@ export function ListarPartidas()
 
 function getJogadores()
 {    
-    var jogador =[""];
-
-    for (let index = 0; index < 7; index++) {
-        jogador.splice(index,0,getJogador(index.toString()))
+    
+    for (let index = 1; index < 8; index++) {
+        listaJogadores.push(getJogador(index.toString()));
     }
 
-    SetListJogadores(jogador);
 }
 
 function onGerarPartidas()
-{
-    getJogadores();    
-    ConfigurarPartidas();    
+{ 
+    setOpen(!open);
+    if(!listaJogadores)
+    {
+        getJogadores();
+    }
+
+    ConfigurarPartidas();
+    MontarDupla();    
+
+    setOpen(false);
 }
 
 function ConfigurarPartidas(){
 
-    var inicioJogadores = [0,1,2,3];
-    var inicioJogadoresFora = [4,5,6];
+    listaContras.splice(0,listaContras.length); 
+    listaContras.push(...[0,1,2,3]);  
+    jogadoresJogando.splice(0,jogadoresJogando.length);
+    jogadoresDeFora.splice(0,jogadoresDeFora.length);
+    jogadoresJogando.push(...[0,1,2,3]);
+    jogadoresDeFora.push(...[4,5,6]);
 
-    for (let index = 0; index < 30; index++) 
+    let proximoJogador = -1;
+    for (let index = 1; index < 6; index++) 
     {
-        if(index==0){
-            SetJogadoresJogando(inicioJogadores)   
-            SetJogadoresDeFora(inicioJogadoresFora)
-        }
-        else{
-            var jogadoresRemovidos = inicioJogadores.splice(0,1);
-            SetJogadoresJogando([]);
-            for (let j = 0; j < inicioJogadoresFora.length; j++) {
-
-                const element = inicioJogadoresFora[j];
-                SetJogadoresJogando(inicioJogadores.splice(1,3,element));                
+    // const[jogadoresJogando,setJogadoresJogando] = useState<number[]>([0,1,2,3]);
+    // const[jogadoresDeFora,SetJogadoresDeFora] = useState<number[]>([4,5,6]);
+        var arrayJogadoresQueFicamDeFora = new Array<number>();
+        if(proximoJogador < 0)
+        {
+            proximoJogador = Math.max(...jogadoresDeFora)+1;
+            if(proximoJogador==7){
+                proximoJogador=0;
             }
-
-            inicioJogadoresFora = jogadoresRemovidos
-            
         }
-        
+        else
+        {
+            proximoJogador +=1;
+        }
+        jogadoresJogando.splice(proximoJogador,1);        
+        jogadoresDeFora.push(proximoJogador);
+        arrayJogadoresQueFicamDeFora.push(...jogadoresJogando);        
+        jogadoresJogando.splice(0,jogadoresJogando.length);
+        //Lista dos jogadores para jogar
+        listaContras.push(...jogadoresDeFora);        
+        // Terminou a partida e sairam
+        jogadoresJogando.push(...jogadoresDeFora);
+        jogadoresDeFora.splice(0,jogadoresDeFora.length);        
+        // Entra os jogadores que estavam de fora.
+        jogadoresDeFora.push(...arrayJogadoresQueFicamDeFora);        
+
     }
 
 }
 
-function SetPartida()
-{
-    // var mensagem = "";    
 
-    // var valor = !jogadoresJogando[1];
-    // if(valor != "undefinied"){
-    //         mensagem = valor?.valueOf() ?? "";
 
-    // }
-
-    
-    // setListaContras([""]);
-        
-}
 
 return(
-    <React.Fragment>
+    <React.Fragment>      
         <div>
-            <h1>Partida 1</h1>   
+            <h1>Lista de Partida do Vôlei dos Amigos !</h1>   
             <Grid xs={8}>
                 <Button variant="outlined" onClick={()=> onGerarPartidas()}>Gerar Partidas</Button>                
-            </Grid> 
-            <Grid xs={4}>
-                <div>
-
-                {listaContras?.map((x)=>(
+            </Grid>  
+           
+     
+            <TableContainer component={Paper}>
+                
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">                
+                <TableBody>
+                {
+                    listaTextNomesDuplas.map((x)=>
+                    <TableRow key={x}>
+                        <TableCell component="th" scope="row">
+                            {x}
+                            </TableCell>
+                    </TableRow>
+                    )
                     
-                        <p>{x}</p>
-                ))}
-                </div>
-
-            </Grid>              
-        </div>
+                }   
+                </TableBody>
+            </Table>
+            </TableContainer>                        
+        </div>            
     </React.Fragment>
 );
 
